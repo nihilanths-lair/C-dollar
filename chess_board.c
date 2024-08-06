595f51f05e1daa3bd1bbdd947922733b3bdb4523
shakhterius <gleb.ovdeenko@gmail.com>
2024-08-06 17:37:07 +1100

    Add files via upload

---
+++ chess_board.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <locale.h>
+//
+char fop(FILE* h);
+//
+int main()
+{
+    setlocale(0, ""); // установим русский язык
+    //
+    FILE* h = NULL;
+    if (fop(h) == -1) return 1;
+    char symbol;
+    if ((symbol = fgetc(h)) == EOF)
+    {
+        printf("Файл пустой.");
+        return 1;
+    }
+    printf("Файл содержит запись.");
+    fclose(h);
+    printf("Файл закрыт.");
+    return 0;
+}
+char fop(FILE* h)
+{
+    h = fopen("compiler.cfg", "rb"); // откроем файл
+    if (h == NULL) // если файл не существует, то
+    {
+        h = fopen("compiler.cfg", "wb"); // создадим его, и
+        if (h == NULL) // снова проверим, что файл создан
+        {
+            printf("Не смог создать файл.");
+            return -1;
+        }
+        printf("Файл создан и открыт на запись.\n");
+        fclose(h);
+        printf("Файл закрыт.\n");
+        h = fopen("compiler.cfg", "rb"); // откроем файл, и считаем конфигурационные данные с него
+        if (h == NULL) // если файл не существует, то
+        {
+            printf("Не смог открыть файл на чтение.");
+            return -1;
+        }
+        printf("Файл переоткрыт на чтение.\n"); // если файл существует, то считаем с него конфигурационные данные
+    }
+    else printf("Файл открыт на чтение.\n"); // если файл существует, то считаем с него конфигурационные данные
+    return 0;
+}
