3ccb69144da55ed5ea8115ec493a7d9392440570
infini-miner <gleb.ovdeenko@gmail.com>
2025-01-03 04:25:53 +1100

    Add files via upload

--- 0.c
+++ 0.c
@@ -3,20 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 // Определение команд
-typedef enum { OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_HALT } Opcode;
+typedef enum { OP_MOV, OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_HALT } Opcode;
 // Структура виртуальной машины (VM) - можно добавить другие поля, например стек
 typedef struct {
     unsigned char *code;
     int pc;
 } VM;
 // Функции, выполняющие команды
-void op_add() { printf("op_add();\n"); }
-void op_sub() { printf("op_sub();\n"); }
-void op_mul() { printf("op_mul();\n"); }
-void op_div() { printf("op_div();\n"); }
-void op_halt() { printf("op_halt();\n"); }
+void op_mov() { printf("op_mov()"); }
+void op_add() { printf("op_add()"); }
+void op_sub() { printf("op_sub();"); }
+void op_mul() { printf("op_mul();"); }
+void op_div() { printf("op_div();"); }
+void op_halt() { printf("op_halt();"); }
 // Массив указателей на функции
-void (*op_table[])() = { op_add, op_sub, op_mul, op_div, op_halt };
+void (*opcode_table[])() = { op_mov, op_add, op_sub, op_mul, op_div, op_halt };
 char byte_code[] = {0xC3, 0xEB, 0x65, 0xE1};
 //
 int main(void)
@@ -29,19 +30,19 @@ int main(void)
     {
         vm.pc = 0;
         vm.code = "\0";
-        printf("vm->pc = %i\n", vm.pc);
-        printf("vm->code = \"%s\"\n", vm.code);
-        printf("vm->code[vm->pc] = \"%s\"\n", vm.code[vm.pc]);
+        printf("vm.pc = %i\n", vm.pc);
+        printf("vm.code = \"%s\"\n", vm.code);
+        printf("vm.code[vm.pc] = \"%s\"\n", vm.code[vm.pc]);
         Opcode opcode = (Opcode) vm.code[vm.pc++]; // Получение кода операции
         /*------------------------------------------------------------------------------------------------*/
         // Проверка на корректность oп-кода, обработка ошибок (использовать только при чистой интерпретации)
-        if (opcode >= sizeof (op_table) / sizeof (op_table[0]))
+        if (opcode >= sizeof (opcode_table) / sizeof (opcode_table[0]))
         {
             fprintf(stderr, "Invalid opcode: %d\n", opcode);
             return EXIT_FAILURE;
         }
         /*------------------------------------------------------------------------------------------------*/
-        op_table[opcode](&vm);
+        opcode_table[opcode](&vm);
         if (opcode == OP_HALT) break;
     }
     return EXIT_SUCCESS;
