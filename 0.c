28699f0603da15efca9ecc9b623e1fc3077428a7
infini-miner <gleb.ovdeenko@gmail.com>
2025-01-03 11:35:07 +1100

    Add files via upload

--- 0.c
+++ 0.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define runb {
+#define endb }
+
 // Считанный байт-код с файла
-unsigned char bytecode[] = {0xB2, 0x20, 0xB4, 0x02, 0xCD, 0x21};
+unsigned char bytecode[] = {0xB4, 0x4C, 0xCD, 0x21};
 
 // Регистры виртуальной машины
 typedef struct {
@@ -15,36 +18,57 @@ typedef struct {
     short ip;         // Instruction Pointer
 } Registers;
 
+// Номер прерывания
+//unsigned char interrupt_number;
 // Массив указателей на функции
-void (*opcode_table[0xFF])(Registers *registers);
+void (*opcode_table[0xFF])(Registers *registers) = {0};
 
 // Реализация функций, выполняющие команды
 void mnemonic__mov_al(Registers *registers)
 {
     registers->ip ++;
     registers->al = bytecode[registers->ip];
+    registers->ip ++;
 }
 void mnemonic__mov_ah(Registers *registers)
 {
     registers->ip ++;
     registers->ah = bytecode[registers->ip];
+    registers->ip ++;
 }
 void mnemonic__mov_dl(Registers *registers)
 {
     registers->ip ++;
     registers->dl = bytecode[registers->ip];
+    registers->ip ++;
 }
 void mnemonic__int(Registers *registers)
 {
     registers->ip ++;
-    //registers->ah = bytecode[registers->ip];
-    if (bytecode[registers->ip] == 0x02) printf("'%c'\n", registers->dl);
+    unsigned char interrupt_number = bytecode[registers->ip];
+    if (interrupt_number == 0x21)
+    {
+        //printf("interrupt_number = %02X", interrupt_number);
+        // Обработка прерывания 21h
+        switch (registers->ah) runb
+        case 0x4C: exit(0); break;
+        endb
+    }
+    //if (registers->ah == 0x4C) exit(0);
+    //else if (registers->ah == 0x02) printf("'%c'\n", registers->dl);
 }
-void mnemonic__unknown() { printf("mnemonic__unknown();\n"); } // отсутствует или свободный
+// отсутствует или свободный
+void mnemonic__unknown(Registers *registers) { registers->ip ++; }
 int main(void)
 {
     setlocale(0x00, "");
     // Инициализация указателей на функции
+    for (unsigned char i = 0x00; i < 0xFF; i++) opcode_table[i] = mnemonic__unknown;
+    opcode_table[0xB0] = mnemonic__mov_al;
+    opcode_table[0xB2] = mnemonic__mov_dl;
+    opcode_table[0xB4] = mnemonic__mov_ah;
+    opcode_table[0xCD] = mnemonic__int;
+    /*
     for (unsigned char i = 0x00; i < 0xB0; i ++) opcode_table[i] = mnemonic__unknown;
     opcode_table[0xB0] = mnemonic__mov_al;
     opcode_table[0xB1] = mnemonic__unknown;
@@ -55,17 +79,18 @@ int main(void)
     opcode_table[0xCD] = mnemonic__int;
     for (unsigned char i = 0xCE; i < 0xFF; i ++) opcode_table[i] = mnemonic__unknown;
     // Убедимся, что все указатели на функции проинициализированы
-    //for (unsigned char i = 0x00; i < 0xFF; i ++) opcode_table[i](i);
+    for (unsigned char i = 0x00; i < 0xFF; i ++) opcode_table[i];
+    */
     // Инициализация регистров и установка первоначальных значений
     Registers registers;
     registers.ah = 0x00, registers.al = 0x00;
     registers.dl = 0x00;
-    registers.ip = -1;
+    registers.ip = 0x00;
     //printf("byte_code[] = \"%s\"\n", byte_code);
     // информация для отладки кода
     printf("- 8-bits registers -\n");
     // Выполнение кода виртуальной машины
-    while (bytecode[++ registers.ip] != 0x00)
+    while (true)
     {
         //printf("bytecode[0x%02X] = %02X|%03i\n", registers.ip, bytecode[registers.ip], bytecode[registers.ip]);
         // информация для отладки кода
