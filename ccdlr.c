34ea4ad77b92232c73dc5d696f20ef645e253bc0
infini-miner <gleb.ovdeenko@gmail.com>
2025-02-19 02:04:24 +1100

    Add files via upload

--- ccdlr.c
+++ ccdlr.c
@@ -7,6 +7,7 @@
 unsigned char __buffer[MAXIMUM_BUFFER_SIZE];
 
 #define strfind strstr
+//#define EOS 0
 
 int main(int argc, unsigned char *argv[])
 {
@@ -21,6 +22,12 @@ int main(int argc, unsigned char *argv[])
         return 1;
     }
     FILE *handle = fopen(argv[1], "rb+"); // .cdlr или .sccdlr
+    if (handle == NULL)
+    {
+        printf("\nНе удалось открыть файл на чтение.");
+        return 2;
+    }
+    for (int i = 0; (__buffer[i] = getc(handle)) != EOF; i ++)
     fclose(handle);
     return 0;
 }
