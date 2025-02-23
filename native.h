70268f7ae588aa2dbe0d72f021014af919018186
infini-miner <gleb.ovdeenko@gmail.com>
2025-02-24 02:11:02 +1100

    Delete native.h

--- native.h
+++
@@ -1,8 +0,0 @@
-int IncrementalProcessing(FILE *handle);
-int NotIncrementalProcessing(FILE *handle);
-
-int LexicalAnalyzer();
-int SyntacticAnalyzer();
-int SemanticAnalyzer();
-int BytecodeGenerator();
-int MachineCodeGenerator();
