a79258d153e28334de1d42da042d3f2a4156267d
infini-miner <gleb.ovdeenko@gmail.com>
2025-04-27 12:45:56 +1100

    Add files via upload

--- executable_file_analyzer.c
+++ executable_file_analyzer.c
@@ -16,106 +16,162 @@ int main(int argc, char *argv[])
     int offset = 0;
     print("                                 ____________");
     print("|-------------------------------/ DOS HEADER \\--------------------------------|");
-    char e_magic[2+1] = {0}; // MZ
-    e_magic[0] = getc(file); // M
-    e_magic[1] = getc(file); // Z
-    fprint("| %03d=%02X | %02X %02X                                        | %s                  |\n", offset, offset, e_magic[0]&255, e_magic[1]&255, e_magic);
-    
-    char e_cblp[2+1] = {0};  // h=02
-    e_cblp[0] = getc(file), e_cblp[1] = getc(file); // Необязательное поле
-    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset, e_cblp[0]&255, e_cblp[1]&255, e_cblp);
-    
-    char e_cp[2+1] = {0};   // h=04
-    e_cp[0] = getc(file), e_cp[1] = getc(file); // Длина образа (страниц)
-    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset, e_cp[0]&255, e_cp[1]&255, e_cp);
-    
-    char e_crlc[2+1] = {0}; // h=06
-    e_crlc[0] = getc(file), e_crlc[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset, e_crlc[0]&255, e_crlc[1]&255, e_crlc);
-    
-    char e_cparhdr[2+1] = {0}; // Длина заголовка в параграфах (выставить значение)
-    e_cparhdr[0] = getc(file), e_cparhdr[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset, e_cparhdr[0]&255, e_cparhdr[1]&255, e_cparhdr);
-    
-    char e_minalloc[2+1] = {0}; // Минимум требуемой памяти (необязательный параметр)
-    e_minalloc[0] = getc(file), e_minalloc[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset, e_minalloc[0]&255, e_minalloc[1]&255, e_minalloc);
-    
-    char e_maxalloc[2+1] = {0}; // Максимум требуемой памяти (выставить значение)
-    e_maxalloc[0] = getc(file), e_maxalloc[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                  |\n", offset += 2, offset, e_maxalloc[0]&255, e_maxalloc[1]&255, e_maxalloc);
-    
-    char e_ss[2+1] = {0}; // Сегмент стека
-    e_ss[0] = getc(file), e_ss[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset, e_ss[0]&255, e_ss[1]&255, e_ss);
-    
-    char e_sp[2+1] = {0}; // Указатель стека
-    e_sp[0] = getc(file), e_sp[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset, e_sp[0]&255, e_sp[1]&255, e_sp);
-    
-    char e_csum[2+1] = {0}; // Необязательный параметр
-    e_csum[0] = getc(file), e_csum[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset, e_csum[0]&255, e_csum[1]&255, e_csum);
-    
-    char e_ip[2+1] = {0}; // Указатель команд
-    e_ip[0] = getc(file), e_ip[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset, e_ip[0]&255, e_ip[1]&255, e_ip);
-    
-    char e_cs[2+1] = {0}; // Необязательный параметр
-    e_cs[0] = getc(file), e_cs[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset, e_cs[0]&255, e_cs[1]&255, e_cs);
-    
-    char e_lfarlc[2+1] = {0}; // Необязательный параметр
-    e_lfarlc[0] = getc(file), e_lfarlc[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset, e_lfarlc[0]&255, e_lfarlc[1]&255, e_lfarlc);
-    
-    char e_ovno[2+1] = {0}; // Необязательный параметр
-    e_ovno[0] = getc(file), e_ovno[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset, e_ovno[0]&255, e_ovno[1]&255, e_ovno);
-
-    char e_res_4_[4*2+1]/*8*/ = {0}; // Необязательный параметр
-    e_res_4_[0] = getc(file), e_res_4_[1] = getc(file),
-    e_res_4_[2] = getc(file), e_res_4_[3] = getc(file),
-    e_res_4_[4] = getc(file), e_res_4_[5] = getc(file),
-    e_res_4_[6] = getc(file), e_res_4_[7] = getc(file);
-    fprint("| %03d=%02X | %02X %02X %02X %02X %02X %02X %02X %02X                      | %s                    |\n", offset += 2, offset, 
-     e_res_4_[0]&255, e_res_4_[1]&255, e_res_4_[2]&255, e_res_4_[3]&255,
-     e_res_4_[4]&255, e_res_4_[5]&255, e_res_4_[6]&255, e_res_4_[7]&255,
+    unsigned char e_magic[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                  |\n", offset, offset,
+     e_magic[0],
+     e_magic[1],
+     e_magic
+    );
+    unsigned char e_cblp[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset,
+     e_cblp[0],
+     e_cblp[1],
+     e_cblp
+    );
+    // Длина образа (страниц)
+    unsigned char e_cp[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset,
+     e_cp[0],
+     e_cp[1],
+     e_cp
+    );
+    unsigned char e_crlc[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset,
+     e_crlc[0],
+     e_crlc[1],
+     e_crlc
+    );
+    // Длина заголовка в параграфах
+    unsigned char e_cparhdr[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset,
+     e_cparhdr[0],
+     e_cparhdr[1],
+     e_cparhdr
+    );
+    // Минимум требуемой памяти
+    unsigned char e_minalloc[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset,
+     e_minalloc[0],
+     e_minalloc[1],
+     e_minalloc
+    );
+    // Максимум требуемой памяти
+    unsigned char e_maxalloc[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                  |\n", offset += 2, offset,
+     e_maxalloc[0],
+     e_maxalloc[1],
+     e_maxalloc
+    );
+    // Сегмент стека
+    unsigned char e_ss[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset,
+     e_ss[0],
+     e_ss[1],
+     e_ss
+    );
+    // Указатель стека
+    unsigned char e_sp[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset,
+     e_sp[0],
+     e_sp[1],
+     e_sp
+    );
+    unsigned char e_csum[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset,
+        e_csum[0],
+        e_csum[1],
+        e_csum
+    );
+    // Указатель команд
+    unsigned char e_ip[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset,
+     e_ip[0],
+     e_ip[1],
+     e_ip
+    );
+    unsigned char e_cs[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset,
+     e_cs[0],
+     e_cs[1],
+     e_cs
+    );
+    unsigned char e_lfarlc[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                   |\n", offset += 2, offset,
+     e_lfarlc[0],
+     e_lfarlc[1],
+     e_lfarlc
+    );
+    unsigned char e_ovno[2+1] = {getc(file), getc(file), '\0'};
+    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset,
+     e_ovno[0],
+     e_ovno[1],
+     e_ovno
+    );
+    unsigned char e_res_4_[4*2+1]/*8*/ =
+    {
+        getc(file), getc(file),
+        getc(file), getc(file),
+        getc(file), getc(file),
+        getc(file), getc(file)
+    };
+    fprint("| %03d=%02X | %02X %02X %02X %02X %02X %02X %02X %02X                      | %s                    |\n", offset += 2, offset,
+     e_res_4_[0],
+     e_res_4_[1],
+     e_res_4_[2],
+     e_res_4_[3],
+     e_res_4_[4],
+     e_res_4_[5],
+     e_res_4_[6],
+     e_res_4_[7],
      e_res_4_
     );
-
-    char e_oemid[2+1] = {0}; // Необязательный параметр
-    e_oemid[0] = getc(file), e_oemid[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 8, offset, e_oemid[0]&255, e_oemid[1]&255, e_oemid);
-
-    char e_oeminfo[2+1] = {0}; // Необязательный параметр
-    e_oeminfo[0] = getc(file), e_oeminfo[1] = getc(file);
-    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset, e_oeminfo[0]&255, e_oeminfo[1]&255, e_oeminfo);
-
-    char e_res2_10_[10*2+1]/*20*/ = {0}; // Необязательный параметр
-    e_res2_10_[0] = getc(file), e_res2_10_[1] = getc(file); //  1  00 00
-    e_res2_10_[2] = getc(file), e_res2_10_[3] = getc(file); //  2  00 00
-    e_res2_10_[4] = getc(file), e_res2_10_[5] = getc(file); //  3  00 00
-    e_res2_10_[6] = getc(file), e_res2_10_[7] = getc(file); //  4  00 00
-    e_res2_10_[8] = getc(file), e_res2_10_[9] = getc(file); //  5  00 00
-    e_res2_10_[10] = getc(file), e_res2_10_[11] = getc(file); //  6  00 00
-    e_res2_10_[12] = getc(file), e_res2_10_[13] = getc(file); //  7  00 00
-    e_res2_10_[14] = getc(file), e_res2_10_[15] = getc(file); //  8  00 00
-    e_res2_10_[16] = getc(file), e_res2_10_[17] = getc(file); //  9  00 00
-    e_res2_10_[18] = getc(file), e_res2_10_[19] = getc(file); // 10  00 00
-    //print("|                                                        +-----------+            |");
+    unsigned char e_oemid[2+1] =
+    {
+        getc(file),
+        getc(file),
+        '\0'
+    };
+    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 8, offset,
+     e_oemid[0],
+     e_oemid[1],
+     e_oemid
+    );
+    unsigned char e_oeminfo[2+1] =
+    {
+        getc(file),
+        getc(file),
+        '\0'
+    };
+    fprint("| %03d=%02X | %02X %02X                                        | %s                    |\n", offset += 2, offset,
+     e_oeminfo[0],
+     e_oeminfo[1],
+     e_oeminfo
+    );
+    unsigned char e_res2_10_[10*2+1]/*20*/ =
+    {
+        getc(file), getc(file), //  1  00 00
+        getc(file), getc(file), //  2  00 00
+        getc(file), getc(file), //  3  00 00
+        getc(file), getc(file), //  4  00 00
+        getc(file), getc(file), //  5  00 00
+        getc(file), getc(file), //  6  00 00
+        getc(file), getc(file), //  7  00 00
+        getc(file), getc(file), //  8  00 00
+        getc(file), getc(file), //  9  00 00
+        getc(file), getc(file)  // 10  00 00
+    };
     fprint("| %03d=%02X | %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X |                     |\n|\
         | %02X %02X %02X %02X %02X                               | %s                    |\n", offset += 2, offset,
-     e_res2_10_[0]&255, e_res2_10_[1]&255, //  1  00 00
-     e_res2_10_[2]&255, e_res2_10_[3]&255, //  2  00 00
-     e_res2_10_[4]&255, e_res2_10_[5]&255, //  3  00 00
-     e_res2_10_[6]&255, e_res2_10_[7]&255, //  4  00 00
-     e_res2_10_[8]&255, e_res2_10_[9]&255, //  5  00 00
-     e_res2_10_[10]&255, e_res2_10_[11]&255, //  6  00 00
-     e_res2_10_[12]&255, e_res2_10_[13]&255, //  7  00 00
-     e_res2_10_[14]&255, e_res2_10_[15]&255, //  8  00 00
-     e_res2_10_[16]&255, e_res2_10_[17]&255, //  9  00 00
-     e_res2_10_[18]&255, e_res2_10_[19]&255, // 10  00 00
+     e_res2_10_ [0], e_res2_10_ [1], //  1  00 00
+     e_res2_10_ [2], e_res2_10_ [3], //  2  00 00
+     e_res2_10_ [4], e_res2_10_ [5], //  3  00 00
+     e_res2_10_ [6], e_res2_10_ [7], //  4  00 00
+     e_res2_10_ [8], e_res2_10_ [9], //  5  00 00
+     e_res2_10_[10], e_res2_10_[11], //  6  00 00
+     e_res2_10_[12], e_res2_10_[13], //  7  00 00
+     e_res2_10_[14], e_res2_10_[15], //  8  00 00
+     e_res2_10_[16], e_res2_10_[17], //  9  00 00
+     e_res2_10_[18], e_res2_10_[19], // 10  00 00
      e_res2_10_
     );
     // Смещение PE-заголовка от начала
