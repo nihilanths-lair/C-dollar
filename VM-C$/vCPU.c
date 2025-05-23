abb3812d1a9d30b0a02c96cbd7316b183a16c1e3
infini-miner <gleb.ovdeenko@gmail.com>
2025-05-24 06:26:40 +1100

    Add files via upload

--- VM-C$/vCPU.c
+++ VM-C$/vCPU.c
@@ -6,34 +6,39 @@
 #include <locale.h>  // Для локализации консоли.
 #include <stdbool.h> // Для использования логических типов: false/true.
 
-char bytecode[] = {0x02, 0x02, 0x02, 0x00};
+char bytecode[] = {0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x00};
 
-short ip = 0x0000;
-short gpr = 0x0000; // general purpose register / регистр общего назначения
+short IP  = 0x0000;
+short GPR = 0x0000; // general purpose register / регистр общего назначения
 
 void Start_vCPU()
 {
     void *instructions[] =
     {
-        &&hlt, // idx:0, elm:1.
-        &&mov, // idx:1, elm:2.
-        &&nop  // idx:2, elm:3.
+        &&HLT, // idx:0, elm:1.
+        &&MOV, // idx:1, elm:2.
+        &&INT, // idx:2, elm:3.
+        &&NOP  // idx:3, elm:4.
     };
     while (true)
     {
-        goto *instructions[bytecode[ip]]; // Аналог *(ptr_label+i).
+        goto *instructions[bytecode[IP]]; // Аналог *(instructions + (*(bytecode + IP))).
         //-------------------------------------------------
-        hlt: // Останавливает выполнение vCPU
-        puts("#DEBUG: HLT - остановить выполнение vCPU.");
+        HLT: // 0 | Останавливает выполнение vCPU
+        puts("#DEBUG: HLT - Остановить выполнение vCPU.");
         break;
         //-------------------------------------------------
-        mov: // Пересылает данные
-        puts("#DEBUG: MOV - перемещает данные между памятью.");
+        MOV: // 1 | Пересылка данных
+        puts("#DEBUG: MOV - Перемещает данные между памятью.");
         continue;
         //-------------------------------------------------
-        nop: // Заглушка
-        puts("#DEBUG: NOP - заглушка");
-        ip++;
+        INT: // 2 | Прерывание
+        puts("#DEBUG: INT - Обращается к таблице векторных прерываний (IVT).");
+        continue;
+        //-------------------------------------------------
+        NOP: // 3 | Заглушка
+        puts("#DEBUG: NOP - Заглушка");
+        IP++;
         continue;
         //-------------------------------------------------
     }
