e001c6d2366ebca1514168ce93b92bfa18cc8e9e
infini-miner <gleb.ovdeenko@gmail.com>
2025-05-24 06:10:22 +1100

    Add files via upload

--- VM-C$/vCPU.c
+++ VM-C$/vCPU.c
@@ -6,7 +6,7 @@
 #include <locale.h>  // Для локализации консоли.
 #include <stdbool.h> // Для использования логических типов: false/true.
 
-char bytecode[] = {0x00, 0x00, 0x00, 0x01};
+char bytecode[] = {0x02, 0x02, 0x02, 0x00};
 
 short ip = 0x0000;
 short gpr = 0x0000; // general purpose register / регистр общего назначения
@@ -15,19 +15,27 @@ void Start_vCPU()
 {
     void *instructions[] =
     {
-        &&nop, // idx:0, elm:1.
-        &&hlt  // idx:1, elm:2.
+        &&hlt, // idx:0, elm:1.
+        &&mov, // idx:1, elm:2.
+        &&nop  // idx:2, elm:3.
     };
     while (true)
     {
         goto *instructions[bytecode[ip]]; // Аналог *(ptr_label+i).
+        //-------------------------------------------------
+        hlt: // Останавливает выполнение vCPU
+        puts("#DEBUG: HLT - остановить выполнение vCPU.");
+        break;
+        //-------------------------------------------------
+        mov: // Пересылает данные
+        puts("#DEBUG: MOV - перемещает данные между памятью.");
+        continue;
+        //-------------------------------------------------
         nop: // Заглушка
         puts("#DEBUG: NOP - заглушка");
         ip++;
         continue;
-        hlt: // Останавливает выполнение vCPU
-        puts("#DEBUG: HLT - остановить выполнение vCPU.");
-        break;
+        //-------------------------------------------------
     }
 }
 int main()
