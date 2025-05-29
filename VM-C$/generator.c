da8d3c6ea108a3eb333424227209ec9b00b5f75d
Глеб Овдеенко / Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2025-05-30 06:45:02 +1100

    Update generator.c

--- VM-C$/generator.c
+++ VM-C$/generator.c
@@ -2,8 +2,41 @@
 
 int main()
 {
-    short count = 2;
+    short count = 255;
     short i;
+    puts("const unsigned char translate_opcode_into_symbolic_form[][8+1] =");
+    puts("{");
+    i = 0;
+    while (i < 176)//0xB0
+    {
+        printf("    \"?\", // %02X %03d\n", i, i);
+        i++;
+    }
+    //printf("    \"\", // %02X %03d\n", i, i);
+    printf("    \"MOV AL\", // %02X %03d\n", i, i);
+    i++;
+    printf("    \"MOV CL\", // %02X %03d\n", i, i);
+    i++;
+    printf("    \"MOV DL\", // %02X %03d\n", i, i);
+    i++;
+    printf("    \"MOV BL\", // %02X %03d\n", i, i);
+    i++;
+    printf("    \"MOV AH\", // %02X %03d\n", i, i);
+    i++;
+    printf("    \"MOV CH\", // %02X %03d\n", i, i);
+    i++;
+    printf("    \"MOV DH\", // %02X %03d\n", i, i);
+    i++;
+    printf("    \"MOV BH\"  // %02X %03d\n", i, i);
+    i = 184;//0xB8
+    while (i < 255)//0xFF
+    {
+        printf("    \"?\", // %02X %03d\n", i, i);
+        i++;
+    }
+    printf("    \"?\"  // %02X %03d\n", i, i);
+    puts("};");
+    /*
     puts("void *instructions[] =");
     puts("{");
     i = 0;
@@ -22,5 +55,6 @@ int main()
         i++;
     }
     printf("__%03d:", i);
+    */
     return 0;
 }
